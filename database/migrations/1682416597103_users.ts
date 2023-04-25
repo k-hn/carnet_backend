29f377a6257e9955ab59ef6cfac5bc1441e3e30f
Hilary <125813796+k-hn@users.noreply.github.com>
2023-04-25 10:03:58 +0000

    [dev] modify user migration and model

--- database/migrations/1682416597103_users.ts
+++ database/migrations/1682416597103_users.ts
@@ -6,9 +6,9 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('email', 255).notNullable().unique()
+      table.string("username", 100).notNullable().unique().index()
+      table.string('email', 255).notNullable().unique().index()
       table.string('password', 180).notNullable()
-      table.string('remember_me_token').nullable()
 
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
